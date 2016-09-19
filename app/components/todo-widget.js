ce35962c9f241b948d50d875e91c5f7c27c23a61
Neha Singh <nehasingh301192@gmail.com>
2016-09-19 13:06:02 +0530

    first commit for emberjs todo app

--- app/components/todo-widget.js
+++ app/components/todo-widget.js
@@ -3,11 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     addTodo(text) {
-      this.get('todos').pushObject({ text: text, id: Date.now()});
+      this.get('todos').pushObject({id: Date.now(), text: text});
     },
     deleteTodo(todo){
-      this.get('todos').removeObject({id: todo.id});
-      console.log(this.get('todos'));
+      this.get('todos').removeObject(todo);
     }
   }
 });
